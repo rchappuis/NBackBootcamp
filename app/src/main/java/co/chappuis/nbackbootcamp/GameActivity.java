ff20e144a5819588981d7c5859b9d3a98db34a44
Reese Chappuis <contact@chappuis.co>
2015-03-25 20:07:24 -0400

    Setting up layout framework

---
+++ app/src/main/java/co/chappuis/nbackbootcamp/GameActivity.java
@@ -0,0 +1,16 @@
+package co.chappuis.nbackbootcamp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by c0dege3k on 3/25/2015.
+ */
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
